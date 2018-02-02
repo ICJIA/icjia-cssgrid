62f889c36edb97f6161e805d4ae676a4234520fb
Chris Schweda <cschweda@gmail.com>
2018-02-02 16:30:47 -0600

    Add navbar to vertical

--- src/pages/Vertical.vue
+++ src/pages/Vertical.vue
@@ -1,6 +1,13 @@
 <template>
     <section class="page-wrapper">
-        <div class="container xl mx-auto">
+        <div class="topnav nav-shadow" id="myTopnav">
+            <a href="#home" class="active">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+            <a style="font-size:15px;" class="icon" v-on:click="showDropdown()">&#9776;</a>
+        </div>
+        <div class="container xl mx-auto mb-8" style="padding-top: 70px">
 
             <div class="body-content">
                 <div class="latest shadow-md pt-8 pl-8 pr-8 pb-8">
@@ -8,7 +15,7 @@
                         <h2>Latest</h2>
                         <a @click.prevent class="link">archive</a>
                     </div>
-                    <div v-for="index in 15" class="pb-4 mt-4 main-item" :key="index">
+                    <div v-for="index in 15" class="pb-4 mt-4 main-item text-base" :key="index">
                         <a @click.prevent class="link">
                             <h4 v-dummy="10"></h4>
                         </a>
@@ -41,8 +48,11 @@
                     </div>
                 </div>
             </div>
-
+            <div class="mt-2 text-sm">Inspired by
+                <a href="http://www.axios.com" class="link">Axios</a>
+            </div>
         </div>
+
     </section>
 </template>
 
@@ -87,7 +97,7 @@
                 } else {
                     pastText = weeksAgo + ' weeks ago'
                 }
-                console.log(el)
+
                 el.innerHTML = pastText;
             })
 
@@ -100,7 +110,15 @@
         },
         methods: {
 
+            showDropdown: function () {
+                var x = document.getElementById("myTopnav");
+                if (x.className === "topnav") {
+                    x.className += " responsive";
+                } else {
+                    x.className = "topnav";
+                }
 
+            }
 
         }
     };
@@ -177,12 +195,93 @@
 
     }
 
+
+
+    .topnav {
+        overflow: hidden;
+        background-color: #fff;
+        top: 0;
+        z-index: 100;
+        position: fixed;
+        width: 100%;
+        border-bottom: 1px solid #ccc;
+        -webkit-box-shadow: 2px 5px 8px -2px rgba(156, 154, 156, 0.47);
+        -moz-box-shadow: 2px 5px 8px -2px rgba(156, 154, 156, 0.47);
+        box-shadow: 2px 5px 8px -2px rgba(156, 154, 156, 0.47);
+
+
+
+
+
+    }
+
+    .topnav a {
+        float: left;
+        display: block;
+        color: #333;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 14px;
+        font-weight: 700;
+        text-transform: uppercase;
+    }
+
+    .topnav a:hover {
+        background-color: #ddd;
+        color: black;
+    }
+
+    .active {
+        background-color: rgb(189, 33, 33);
+        color: white !important;
+    }
+
+    .topnav .icon {
+        display: none;
+    }
+
+    .nav-shadow {}
+
+
+
+    @media screen and (max-width: 700px) {
+        .topnav a:not(:first-child) {
+            display: none;
+        }
+        .topnav a.icon {
+            float: right;
+            display: block;
+        }
+    }
+
+    @media screen and (max-width: 700px) {
+        .topnav.responsive {
+            position: relative;
+
+        }
+        .topnav.responsive .icon {
+            position: absolute;
+            right: 0;
+            top: 0;
+        }
+        .topnav.responsive a {
+            float: none;
+            display: block;
+            text-align: left;
+
+        }
+
+    }
+
     @media (max-width: 700px) {
         .body-content {
             display: grid;
             grid-auto-rows: minmax(200px, auto);
             grid-template-columns: 1fr;
             grid-gap: 5px;
+            padding-left: 15px;
+            padding-right: 15px;
 
         }
     }
